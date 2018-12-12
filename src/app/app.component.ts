0b53a4ba7fad1cbdce687631a540f7b673350a50
apiched1 <apiched_oh@hotmail.com>
2018-12-12 14:51:20 +0700

    "implement total score page"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { ElectoratePage } from '../pages/electorate/electorate';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = ElectoratePage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ElectoratePage } from '../electorate/electorate';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,7 +11,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = ElectoratePage;
+  
 
   constructor() {
 
