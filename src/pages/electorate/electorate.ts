8ffbec7817d24a5c535ed3536adaec86d5bb2134
thaninpong <thaninpong@perfenterprise.com>
2018-12-22 13:16:55 +0700

    Add API

--- src/pages/electorate/electorate.ts
+++ src/pages/electorate/electorate.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
-import { AreaElectionPage } from '../area-election/area-election';
-import { NumberValueAccessor } from '@angular/forms/src/directives';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the ElectoratePage page.
@@ -18,21 +17,33 @@ import { NumberValueAccessor } from '@angular/forms/src/directives';
 export class ElectoratePage {
 
   areaPolitical: string;
+  token: Object;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheetCtrl: ActionSheetController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheetCtrl: ActionSheetController,public http: HttpClient) {
+    this.http.get("http://pbiebeded.azurewebsites.net/api/values").subscribe(
+      it => {
+        this.token = it
+        console.log("this.token");
+        console.log(this.token);
+      }
+    );
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ElectoratePage');
   }
 
-  onClick(event) {
+  onClick(event,tokens) {
     console.log(event);
     var target = event.target || event.srcElement || event.currentTarget;
     var idAttr = target.offsetParent.id;
     this.areaPolitical = idAttr;
-    this.navCtrl.push("AreaElectionPage", { _areaPolitical: this.areaPolitical });
+    this.token = tokens
+    this.navCtrl.push("AreaElectionPage", { 
+      _areaPolitical: this.areaPolitical,
+      tokenid: tokens
+       
+    });
     console.log(idAttr);
   }
 
