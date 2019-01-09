d39952732bc76f9fbc549907822729fa0a53c60d
guntza22 <guntza22@hotmail.com>
2019-01-09 10:25:39 +0700

    implement global variable hostGetToken,groupid,reportid"

--- src/app/model.ts
+++ src/app/model.ts
@@ -13,4 +13,7 @@ export class ElectionModel {
 
 export class GlobalVaraible {
     static host: string = "http://electionvars.azurewebsites.net/api/Election/";
+    static hostGetToken : string = "http://pbiebeded.azurewebsites.net/api/values/gettoken/";
+    static groupid : string = "45f95249-7ae8-4335-899b-d66de3334065";
+    static reportid : string = "e00e412c-d883-4fdb-a6eb-9bb3f699c1e2";
 }
