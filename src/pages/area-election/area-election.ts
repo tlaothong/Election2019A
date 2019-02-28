e927c319ddc7158831393a24e6400a50c1b9dae0
thaninpong <thaninpong@perfenterprise.com>
2019-02-28 16:05:29 +0700

    Add Color Graph

--- src/pages/area-election/area-election.ts
+++ src/pages/area-election/area-election.ts
@@ -28,7 +28,7 @@ export class AreaElectionPage {
     this.data = this.navParams.get('_area');
     console.log("this.data");
     console.log(this.data);
-    
+
     // this.namekad = this.navParams.get('nameArea');
   }
 
@@ -38,12 +38,61 @@ export class AreaElectionPage {
     this.http.get<ScoreArea[]>(GlobalVaraible.host + "GetAreaTable2/" + this.area.idArea).subscribe(
       data => {
         this.listScoreParty = data;
+        let xxx = this.listScoreParty;
         console.log(this.listScoreParty);
         this.listOther = this.listScoreParty.slice(5);
         this.other = { name: "อื่นๆ", score: 0 };
         this.listOther.forEach(data => {
           this.other.score += data.score;
         });
+
+
+        function getRandomColorHex(index) {
+          for (let i = 0; i < xxx.length; i++) {
+            var color = "";
+            if (xxx[index].nameInitial == "อ.น.ค.") {
+              color = "#FF7F00"
+            }else if (xxx[index].nameInitial == "ภท.") {
+              color = "#00008B"
+            }else if(xxx[index].nameInitial =="ปชป."){
+              color = "#00BFFF"
+            }else if(xxx[index].nameInitial =="ทษช."){
+              color = "#0000EE"
+            }else if(xxx[index].nameInitial =="สร."){
+              color = "#27408B"
+            }else if(xxx[index].nameInitial =="พท."){
+              color = "#FF0000"
+            }else if(xxx[index].nameInitial =="พปชร."){
+              color = "#4876FF"
+            }else if(xxx[index].nameInitial =="รปช."){
+              color = "#0000FF"
+            }else if(xxx[index].nameInitial =="ชทพ."){
+              color = "#FF69B4"
+            }else if(xxx[index].nameInitial =="พช."){
+              color = "#CD0000"
+            }else if(xxx[index].nameInitial =="ปชช."){
+              color = "#FFFF00"
+            }else if(xxx[index].nameInitial =="ปช."){
+              color = "#CD0000"
+            }else {
+              color = "#7F7E7F"
+            }
+            // else if (xxx[i].nameInitial == "ชทพ.") {
+            //   color = "#00008B"
+            // } else if (xxx[i].nameInitial == "ปชป.") {
+            //   color = "#00BFFF"
+            // } else {
+            //   color = "#FF33FE"
+            // }
+            console.log("color");
+            console.log(color);
+            console.log("xxxx");
+            console.log(xxx[index]);
+            return color;
+          }
+        }
+
+
         this.chart = new Chart(this.barCanvas.nativeElement, {
           type: 'bar',
           data: {
@@ -54,12 +103,12 @@ export class AreaElectionPage {
               data: [this.listScoreParty[0].score, this.listScoreParty[1].score, this.listScoreParty[2].score
                 , this.listScoreParty[3].score, this.listScoreParty[4].score, this.other.score],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                getRandomColorHex(0),
+                getRandomColorHex(1),
+                getRandomColorHex(2),
+                getRandomColorHex(3),
+                getRandomColorHex(4),
+                "#7F7E7F",
               ],
               borderColor: [
                 'rgba(255,99,132,1)',
@@ -83,6 +132,8 @@ export class AreaElectionPage {
           }
         });
       });
+
+
   }
 }
 
