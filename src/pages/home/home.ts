e7bca93b921a83dd0e4f664f74665778b5f3148e
guntza22 <guntza22@hotmail.com>
2019-03-10 00:36:06 +0700

    resolve for update api 3/10/2019 12.36

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
-import { ScoreParty, GlobalVaraible } from '../../app/model';
+import { ScoreParty, GlobalVaraible, ScoreOther } from '../../app/model';
 import { PoliticalPartyScorePage } from '../political-party-score/political-party-score';
 import { KadGraphPage } from '../kad-graph/kad-graph';
 import { PartyGraphPage } from '../party-graph/party-graph';
@@ -14,20 +14,39 @@ import { TeamPage } from '../team/team';
 export class HomePage {
 
   listScoreParty: ScoreParty[];
+  listScoreParty2: ScoreParty[];
+  listScoreOther: ScoreParty[];
+  otherScore: ScoreOther = new ScoreOther;
   urlImg: string;
   constructor(public navCtrl: NavController, public http: HttpClient) {
   }
 
   ionViewDidEnter() {
     this.listScoreParty = [];
+    this.listScoreParty2 = [];
     this.http.get<ScoreParty[]>(GlobalVaraible.host + "GetApp1AllScoreParty")
       .subscribe(data => {
         this.listScoreParty = data;
+        this.listScoreParty2 = this.listScoreParty.filter(it => it.idParty == "034" || it.idParty == "077" || it.idParty == "001"
+          || it.idParty == "192" || it.idParty == "177" || it.idParty == "063"
+          || it.idParty == "055" || it.idParty == "145" || it.idParty == "181"
+          || it.idParty == "149" || it.idParty == "176" || it.idParty == "187");
         console.log(this.listScoreParty);
-        this.listScoreParty.forEach(data => {
+        this.listScoreParty2.forEach(data => {
           data.urlImg = "../../assets/imgs/" + data.idParty + ".png";
         });
       });
+    this.listScoreOther = this.listScoreParty.filter(it => it.idParty != "034" && it.idParty != "077" && it.idParty != "001"
+      && it.idParty != "192" && it.idParty != "177" && it.idParty != "063"
+      && it.idParty != "055" && it.idParty != "145" && it.idParty != "181"
+      && it.idParty != "149" && it.idParty != "176" && it.idParty != "187");
+    this.otherScore = { haveScore: 0, scoreArea: 0, scorePartyList: 0, scorePercent: 0, isChecked: true, status: true };
+    this.listScoreOther.forEach(data => {
+      this.otherScore.haveScore += data.haveScore;
+      this.otherScore.scoreArea += data.areaScore;
+      this.otherScore.scorePartyList += data.nameListScore;
+      this.otherScore.scorePercent += data.percentScore;
+    });
   }
 
   goHaveScore() {
@@ -39,7 +58,7 @@ export class HomePage {
   goPartyListGraph() {
     this.navCtrl.push(PartyGraphPage, { _listScoreParty: this.listScoreParty });
   }
-  goTeam(){
+  goTeam() {
     this.navCtrl.push(TeamPage, { _listScoreParty: this.listScoreParty });
   }
 }
